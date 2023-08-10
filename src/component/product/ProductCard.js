8a9b73a59331eab8a950fbbd9db7669803a15e49
Hassanhashmi-475 <hassan.uet475@gmail.com>
2023-08-10 13:34:02 +0500

    issues Fix related to backend on product schema issues card

--- src/component/product/ProductCard.js
+++ src/component/product/ProductCard.js
@@ -36,7 +36,7 @@ const ProductCard = ({ product, attributes }) => {
       toast("Please Login First")
     }
     else {
-    if (p.stock < 1) return notifyError("Insufficient stock!");
+    if (p.quantity < 1) return notifyError("Insufficient stock!");
 
     if (p?.variants?.length > 0) {
       setModalOpen(!modalOpen);
@@ -49,8 +49,8 @@ const ProductCard = ({ product, attributes }) => {
       title: showingTranslateValue(p?.title, lang),
       id: p._id,
       variant: p.prices,
-      price: p.prices.price,
-      originalPrice: product.prices?.originalPrice,
+      price: p.unitPrice,
+      originalPrice: product.unitPrice,
     };
     addItem(newItem);}
   };
@@ -71,54 +71,54 @@ const ProductCard = ({ product, attributes }) => {
         />
       )}
 
-      <div className="group box-border overflow-hidden flex rounded-md shadow-sm pe-0 flex-col items-center bg-white relative">
+      <div className='group box-border overflow-hidden flex rounded-md shadow-sm pe-0 flex-col items-center bg-white relative'>
         <div
           onClick={() => handleModalOpen(!modalOpen, product._id)}
-          className="relative flex justify-center w-full cursor-pointer pt-2"
+          className='relative flex justify-center w-full cursor-pointer pt-2'
         >
-          <div className="left-3">
-            <Stock product={product} stock={product.stock} card />
+          <div className='left-3'>
+            <Stock product={product} stock={product.quantity} card />
           </div>
 
           <Discount product={product} />
 
-          {product?.image[0] ? (
+          {product?.galleryImages[5] ? (
             <Image
-              src={product.image[0]}
+              src={product.galleryImages[5]}
               width={210}
               height={210}
-              alt="product"
-              className="object-contain  transition duration-150 ease-linear transform group-hover:scale-105"
+              alt='product'
+              className='object-contain  transition duration-150 ease-linear transform group-hover:scale-105'
             />
           ) : (
             <Image
-              src="https://res.cloudinary.com/ahossain/image/upload/v1655097002/placeholder_kvepfp.png"
+              src='https://res.cloudinary.com/ahossain/image/upload/v1655097002/placeholder_kvepfp.png'
               width={210}
               height={210}
-              alt="product"
-              className="object-cover transition duration-150 ease-linear transform group-hover:scale-105"
+              alt='product'
+              className='object-cover transition duration-150 ease-linear transform group-hover:scale-105'
             />
           )}
         </div>
-        <div className="w-full px-3 lg:px-4 pb-4 overflow-hidden">
-          <div className="relative mb-1">
-            <span className="text-gray-400 font-medium text-xs d-block mb-1">
+        <div className='w-full px-3 lg:px-4 pb-4 overflow-hidden'>
+          <div className='relative mb-1'>
+            <span className='text-gray-400 font-medium text-xs d-block mb-1'>
               {product.unit}
             </span>
-            <h2 className="text-heading truncate mb-0 block text-sm font-medium text-gray-600">
-              <span className="line-clamp-2">
+            <h2 className='text-heading truncate mb-0 block text-sm font-medium text-gray-600'>
+              <span className='line-clamp-2'>
                 {showingTranslateValue(product?.title, lang)}
               </span>
             </h2>
           </div>
 
-          <div className="flex justify-between items-center text-heading text-sm sm:text-base space-s-2 md:text-base lg:text-xl">
+          <div className='flex justify-between items-center text-heading text-sm sm:text-base space-s-2 md:text-base lg:text-xl'>
             <Price
               card
               product={product}
               currency={currency}
-              price={product?.prices?.price}
-              originalPrice={product?.prices?.originalPrice}
+              price={product?.unitPrice}
+              originalPrice={product?.unitPrice}
             />
 
             {inCart(product._id) ? (
@@ -128,18 +128,18 @@ const ProductCard = ({ product, attributes }) => {
                     item.id === product._id && (
                       <div
                         key={item.id}
-                        className="h-9 w-auto flex flex-wrap items-center justify-evenly py-1 px-2  bg-emerald-500  text-white rounded"
+                        className='h-9 w-auto flex flex-wrap items-center justify-evenly py-1 px-2  bg-emerald-500  text-white rounded'
                       >
                         <button
                           onClick={() =>
                             updateItemQuantity(item.id, item.quantity - 1)
                           }
                         >
-                          <span className="text-dark text-base">
+                          <span className='text-dark text-base'>
                             <IoRemove />
                           </span>
                         </button>
-                        <p className="text-sm text-dark px-1 font-serif font-semibold">
+                        <p className='text-sm text-dark px-1 font-serif font-semibold'>
                           {item.quantity}
                         </p>
                         <button
@@ -149,31 +149,31 @@ const ProductCard = ({ product, attributes }) => {
                               : handleIncreaseQuantity(item)
                           }
                         >
-                          <span className="text-dark text-base">
+                          <span className='text-dark text-base'>
                             <IoAdd />
                           </span>
                         </button>
                       </div>
                     )
-                )}{" "}
+                )}{' '}
               </div>
             ) : (
               <button
                 onClick={() => handleAddItem(product)}
-                aria-label="cart"
-                className="h-9 w-9 flex items-center justify-center border border-gray-200 rounded  text-green-500  hover:border-emerald-500 hover: bg-emerald-500  hover:text-white transition-all"
+                aria-label='cart'
+                className='h-9 w-9 flex items-center justify-center border border-gray-200 rounded  text-green-500  hover:border-emerald-500 hover: bg-emerald-500  hover:text-white transition-all'
               >
-                {" "}
-                <span className="text-xl">
+                {' '}
+                <span className='text-xl'>
                   <IoBagAddSharp />
-                </span>{" "}
+                </span>{' '}
               </button>
             )}
           </div>
         </div>
       </div>
     </>
-  );
+  )
 };
 
 export default dynamic(() => Promise.resolve(ProductCard), { ssr: false });
